44fa6291308b06a51bc744bca4bd7e134a9a9955
Hugo <espenanhugo@gmail.com>
2023-03-17 16:52:18 +0100

    fix: bug

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ include("head.php");
                             </div>
                         </div>
                     </div>
-                    <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
+                    <div class="hero__item set-bg" data-setbg="">
                         <div class="hero__text">
                             <span>FRUIT FRESH</span>
                             <h2>Vegetable <br />100% Organic</h2>

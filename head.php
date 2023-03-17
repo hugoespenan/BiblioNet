179d7a8a5fc840acaf08be30ead5e3caff1f0583
Hugo <espenanhugo@gmail.com>
2023-03-17 16:31:51 +0100

    include: du head dans toutes les pages

--- head.php
+++ head.php
@@ -33,7 +33,7 @@
 <div class="humberger__menu__overlay"></div>
 <div class="humberger__menu__wrapper">
     <div class="humberger__menu__logo">
-        <a href="#"><img src="img/logo.png" alt=""></a>
+        <a href="#"><img src="img/BiblioNet-removebg-preview.png" alt=""></a>
     </div>
     <div class="humberger__menu__cart">
         <ul>
@@ -58,18 +58,16 @@
     </div>
     <nav class="humberger__menu__nav mobile-menu">
         <ul>
-            <li class="active"><a href="./index.html">Home</a></li>
-            <li><a href="./shop-grid.html">Shop</a></li>
+            <li class="active"><a href="./index.php">Home</a></li>
+            <li><a href="./shop-grid.php">Shop</a></li>
             <li><a href="#">Pages</a>
                 <ul class="header__menu__dropdown">
-                    <li><a href="./shop-details.html">Shop Details</a></li>
-                    <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                    <li><a href="./checkout.html">Check Out</a></li>
-                    <li><a href="./blog-details.html">Blog Details</a></li>
+                    <li><a href="./shop-details.php">Shop Details</a></li>
+                    <li><a href="./shoping-cart.php">Shoping Cart</a></li>
+                    <li><a href="./checkout.php">Check Out</a></li>
                 </ul>
             </li>
-            <li><a href="./blog.html">Blog</a></li>
-            <li><a href="./contact.html">Contact</a></li>
+            <li><a href="./contact.php">Contact</a></li>
         </ul>
     </nav>
     <div id="mobile-menu-wrap"></div>
@@ -130,7 +128,7 @@
         <div class="row">
             <div class="col-lg-3">
                 <div class="header__logo">
-                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    <a href="./index.php"><img src="img/BiblioNet-removebg-preview.png" alt=""></a>
                 </div>
             </div>
             <div class="col-lg-6">

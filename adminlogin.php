ef339a6315f78f90cf0c0f3034ba193c13a84d53
Y4CNN <boualiyacine15@gmail.com>
2023-03-30 22:59:04 +0200

    [Ajout] des fichiers php qui serviront d'interface a l'admin

---
+++ adminlogin.php
@@ -0,0 +1,108 @@
+<?php
+
+include("src/vue/head.php");
+include("src/traitement/Inscrit.php");
+include("src/vue/login.php");
+include("src/traitement/Admin.php");
+
+
+if (!isset($_SESSION['admin'])) {
+    $_SESSION['admin'] = false;
+}
+
+
+if (
+    !empty($_POST['email']) and
+    !empty($_POST['mdp'])
+) {
+
+    $admin = new Admin();
+    try {
+        if (1) {
+            $admin->connexion($_POST['email'], $_POST['mdp']);
+        } else {
+            throw new Exception("Un probleme s'est produit lors de votre connexion!!! Merci de ressayer plus tard");
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets/css/theme.css"/>
+    <script type="text/javascript"></script>
+    <title>BiblioNet Connexion</title>
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="header__logo">
+                <a href="index.php">
+                    <img src="assets/img/BiblioNet.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-7">
+            <nav class="header__menu">
+
+                <h3>Connexion en tant qu'Administrateur</h3>
+
+            </nav>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-lg-12">
+
+
+        <?php if ($_SESSION['admin'] == false){ ?>
+        <div class="panel panel-success panel1">
+            <div class="panel-body">
+                <fieldset>
+
+                    <form action="adminlogin.php" method="post">
+                        <table class="login_table">
+
+                            <tr>
+                                <td>Email<span>*</span></td>
+                                <td><input type="text" name="email" id="email" placeholder="Email" required></td>
+                            </tr>
+
+                            <tr>
+                                <td>Mot de passe<span>*</span></td>
+                                <td><input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required></td>
+                            </tr>
+
+                            <tr>
+                                <td><small>Se souvenir </small><input type="checkbox" name="keep" value="true"></td>
+                                <td><input type="submit" value="Connexion"/><input type="reset" value="Effacer"/></td>
+                            </tr>
+                        </table>
+                    </form>
+
+                </fieldset>
+            </div>
+            <?php }
+
+            if ($_SESSION['admin'] == true) {
+                header("Location: adminpage1.php");
+
+            } ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>

4bd09407ea46c79c7718e7a56e039d54fa163fbd
Hugo <espenanhugo@gmail.com>
2023-05-13 14:56:59 +0200

    [Ajout fonctionnalité gestion emprunt]

--- gestionemprunt.php
+++ gestionemprunt.php
@@ -15,5 +15,173 @@
     include("adminpage1.php");
     ?>
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            <div class="tableau">
+
+
+                <div class="panel panel-default">
+                    <div class="panel-body">
+
+                    </div>
+                    <form method="post">
+                        <table class="table">
+
+
+                            <tr>
+                                <th>Emprunt
+                                </th>
+                                <th>Titre du livre emprunté
+                                </th>
+                                <th>Id de l'utilisateur qui a emprunté
+                                </th>
+                                <th>Date emprunt
+                                </th>
+                                <th>Délais
+                                </th>
+                                <th>
+                                </th>
+                                <th>
+                                    <button name="ajoutemprunt" class="btn btn-outline-info" type="submit">Ajouter un
+                                        Emprunt
+                                    </button>
+
+                                </th>
+                            </tr>
+
+
+                            <?php
+                            require_once 'src/traitement/EmpruntController.php';
+                            $emprunt = new EmpruntController();
+                            $resultat = $emprunt->getAll();
+                            $emp = 0;
+                            foreach ($resultat as $emprunt) {
+                                $idemprunt = $emprunt['id_emprunt'];
+                                echo "<tr>" .
+                                    "<td>" . $emprunt['id_emprunt'] .
+                                    "</td><td>" . $emprunt['titre'] .
+                                    "</td><td>" . $emprunt['inscrit'] .
+                                    "</td><td>" . $emprunt['date'] .
+                                    "</td><td>" . $emprunt['delais'] . " jours " .
+                                    "</td><td>" . '<button value="' . $idemprunt . '" name="editemprunt" class="btn btn-outline-primary">Editer</button>' .
+                                    "</td><td>" . '<button value="' . $idemprunt . '" name="deleteemprunt" class="btn btn-outline-danger" type="submit">Supprimer</button>' .
+                                    "</td>" .
+                                    "</tr>";
+                            }
+                            if (isset($_POST['ajoutemprunt'])) {
+                                ?>
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Ajout Livre</h5>
+                                            <button type="submit" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlInput1">Date</label>
+                                                    <input type="date" class="form-control"
+                                                           name="dateaj">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlInput1">Delais</label>
+                                                    <input type="number" class="form-control" placeholder="ex: 1997"
+                                                           name="delaisaj">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlTextarea1">id inscrit</label>
+                                                    <textarea class="form-control"
+                                                              name="ref_inscrit"></textarea>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlTextarea1">id exemplaire</label>
+                                                    <textarea class="form-control"
+                                                              name="ref_exemplaire"></textarea>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-outline-dark" data-dismiss="modal">
+                                                Annuler
+                                            </button>
+                                            <button type="submit" name="ajouter" class="btn btn-outline-primary">
+                                                Ajouter
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+
+                            }
+                            if (isset($_POST['ajouter'])) {
+                                if (isset($_POST['dateaj']) and isset($_POST['delaisaj']) and isset($_POST['ref_exemplaire']) and isset($_POST['ref_inscrit'])){
+                                    $emprunter = new EmpruntController();
+                                    $emprunter->Emprunter($_POST['dateaj'], $_POST['delaisaj'], $_POST['ref_exemplaire'], $_POST['ref_inscrit']);
+                                }
+                            }
+                            if (isset($_POST['deleteemprunt'])) {
+                                $emp = $_POST['deleteemprunt'];
+                                $emprunt0 = new EmpruntController();
+                                $emprunt0->deleteEmrunt($emp);
+                            }
+
+                            if (isset($_POST['editemprunt'])) {
+                                $emp = $_POST['editemprunt'];
+                                ?>
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modification d'un
+                                                emprunt</h5>
+                                            <button type="submit" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form method="post">
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlInput1">Nouveau delais</label>
+                                                    <input type="text" class="form-control"
+                                                           placeholder="ex: 10" name="newdelais">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlInput1">Nouvelle date</label>
+                                                    <input type="date" class="form-control"
+                                                           placeholder="ex: 2023-02-23"
+                                                           name="newdate">
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-secondary" data-dismiss="modal">
+                                                Annuler
+                                            </button>
+                                            <button value="<?php echo $emp ?>" type="submit" name="modifi"
+                                                    class="btn btn-primary">Enregistrer
+                                                les
+                                                changements
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                            if (isset($_POST['modifi'])) {
+                                $emprunt2 = new EmpruntController();
+                                $emprunt2->modifierEmprunt($_POST['modifi'], $_POST['newdelais'], $_POST['newdate']);
+                            }
+                            ?>
+                        </table>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
--- src/traitement/EmpruntController.php
+++ src/traitement/EmpruntController.php
@@ -12,6 +12,8 @@ class EmpruntController
         $bdd = new bdd("biblionet", "localhost", "", "root");
         $requ = $bdd->b->prepare("INSERT INTO emprunt(date, delais, ref_exemplaire, ref_inscrit) VALUES (:date, :delais, :ref_e, :ref_i)");
         $requ->execute(array('date' => $date, 'delais' => $delais, 'ref_e' => $ref_exemplaire, 'ref_i' => $ref_inscrit));
+        $requ2 = $bdd->b->prepare("UPDATE exemplaire SET disponible = true WHERE exemplaire.id_exemplaire = :ref");
+        $requ2->execute(array('ref' => $ref_exemplaire));
     }
 
     public function prolonger($nb_jours, $id_emprunt)
@@ -28,45 +30,84 @@ class EmpruntController
         $requ->execute(array('id' => $id_emprunt));
         $res = $requ->fetch();
         $ret = "";
-        if (!empty($res)){
+        if (!empty($res)) {
             $ret = $res['titre'];
         }
         return $ret;
     }
-    public function getExemplaire($id_emprunt){
+
+    public function getExemplaire($id_emprunt)
+    {
         $bdd = new bdd("biblionet", "localhost", "", "root");
         $requ = $bdd->b->prepare("SELECT exemplaire.id_exemplaire FROM emprunt LEFT JOIN exemplaire ON emprunt.ref_exemplaire = exemplaire.id_exemplaire WHERE id_emprunt = :id");
         $requ->execute(array('id' => $id_emprunt));
         $res = $requ->fetch();
         $ret = "";
-        if (!empty($res)){
+        if (!empty($res)) {
             $ret = $res['id_exemplaire'];
         }
         return $ret;
     }
-    public function NbJoursDepasse($id_emprunt){
+
+    public function NbJoursDepasse($id_emprunt)
+    {
         $bdd = new bdd("biblionet", "localhost", "", "root");
         $requ = $bdd->b->prepare("SELECT (CURRENT_DATE-DATE_ADD(emprunt.date, INTERVAL emprunt.delais DAY)) AS temps FROM emprunt WHERE id_emprunt = :id");
         $requ->execute(array('id' => $id_emprunt));
         $res = $requ->fetch();
         return $res['temps'];
     }
-    public function rendreEmprunt($id_emprunt){
+
+    public function rendreEmprunt($id_emprunt)
+    {
         $bdd = new bdd("biblionet", "localhost", "", "root");
         $requ = $bdd->b->prepare("DELETE FROM emprunt WHERE id_emprunt = :id");
         $requ->execute(array('id' => $id_emprunt));
         $requ2 = $bdd->b->query("UPDATE exemplaire LEFT JOIN emprunt ON emprunt.ref_exemplaire = exemplaire.id_exemplaire SET disponible = true WHERE emprunt.ref_exemplaire = id_exemplaire");
     }
-    public function estDispo($id_livre, $id_edition){
+
+    public function estDispo($id_livre, $id_edition)
+    {
         $bdd = new bdd("biblionet", "localhost", "", "root");
         $requ = $bdd->b->prepare("SELECT * FROM exemplaire WHERE ref_edition = :edit and ref_livre = :livre and disponible = true");
         $requ->execute(array('edit' => $id_edition, 'livre' => $id_livre));
         $res = $requ->fetchAll();
         $ret = false;
-        if (!empty($res)){
+        if (!empty($res)) {
             $ret = true;
         }
         return $ret;
+    }
+
+    public function getAll()
+    {
+        $bdd = new bdd("biblionet", "localhost", "", "root");
+        $requ = $bdd->b->query("SELECT id_emprunt, livre.titre AS titre, ref_inscrit AS inscrit, date, delais FROM emprunt LEFT JOIN exemplaire ON exemplaire.id_exemplaire = emprunt.ref_exemplaire LEFT JOIN livre ON exemplaire.ref_livre = livre.id_livre ORDER BY ref_inscrit;");
+        $res = $requ->fetchAll();
+        return $res;
+    }
+
+    public function deleteEmrunt($id_emprunt)
+    {
+        $bdd = new bdd("biblionet", "localhost", "", "root");
+        $requ = $bdd->b->prepare("DELETE FROM emprunt WHERE id_emprunt = :id");
+        $requ->execute(array('id' => $id_emprunt));
+        $requ2 = $bdd->b->query("UPDATE exemplaire LEFT JOIN emprunt ON emprunt.ref_exemplaire = exemplaire.id_exemplaire SET disponible = true WHERE emprunt.ref_exemplaire = id_exemplaire");
+    }
+
+    public function modifierEmprunt($id_emprunt, $newdelais, $newdate)
+    {
+        $bdd = new bdd("biblionet", "localhost", "", "root");
+        if (!empty($id_emprunt)) {
+            if (!empty($newdate)) {
+                $requ1 = $bdd->b->prepare("UPDATE emprunt SET date = :new WHERE id_emprunt = :id");
+                $requ1->execute(array('new' => $newdate, 'id' => $id_emprunt));
+            }
+            if (!empty($newdelais)) {
+                $requ = $bdd->b->prepare("UPDATE emprunt SET delais = :new  WHERE id_emprunt = :id");
+                $requ->execute(array('new' => $newdelais, 'id' => $id_emprunt));
+            }
+        }
     }
 
 }
